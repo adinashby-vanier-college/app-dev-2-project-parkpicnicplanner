d2fecda4d03c9b5293b65fb81b5edf157c42c971
kaurinz <906530+kaurinz@users.noreply.github.com>
2025-08-30 12:30:48 -0400

    minor: add picnic service to service_locator

--- picnic/lib/services/service_locator.dart
+++ picnic/lib/services/service_locator.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:picnic/services/picnic_service.dart';
 
 import 'picnic_participant_service.dart';
 import 'chat_service.dart';
@@ -8,4 +9,5 @@ final getIt = GetIt.instance;
 void setupServices() {
   getIt.registerLazySingleton<ChatService>(() => ChatService());
   getIt.registerLazySingleton<PicnicParticipantService>(()=> PicnicParticipantService());
+  getIt.registerLazySingleton<PicnicService>(() => PicnicService());
 }
